8a4a37601864dd18c47d5a08844bf2ec6b3e5274
Talor Samara <Talor.Samara@sapiens.com>
2021-10-06 13:58:57 +0300

    my changes

--- test/test.js
+++ test/test.js
@@ -38,7 +38,7 @@ describe('Testing RESTful API', function() {
 
     
     describe("GET /pets", function() {
-        it("responds with all pets", function(done) {
+        it("responds with all pets",async function() {
             chai.request(app)
             .get('/api/pets??page=5&limit=10')
             .set('x-access-token', token)
@@ -47,13 +47,12 @@ describe('Testing RESTful API', function() {
                 res.body.should.be.a('Object');
                 res.body.results.should.be.a('Array');
                 expect(res.body.results).to.have.lengthOf(10);
-                done()
             });
         });
     });
 
     describe("POST /pet", function() {
-        it("responds with a new pet", function(done) {
+        it("responds with a new pet",async function() {
             chai.request(app)
             .post('/api/pet')
             .set('content-type', 'application/x-www-form-urlencoded')
@@ -66,26 +65,25 @@ describe('Testing RESTful API', function() {
             .end((err, res) => {
                 res.should.have.status(200);
                 expect(res.body).to.have.string('added');
-                done();
+                
             });
         });
     });
 
     describe("DELETE /pet", function() {
-        it("responds with deleted pet", function(done) {
+        it("responds with deleted pet",async function() {
             chai.request(app)
             .delete('/api/pet?name=talor')
             .set('x-access-token', token)
             .end((err, res) => {
                 res.should.have.status(200);
                 expect(res.body).to.have.string('deleted');
-                done();
             });
         });
     });
     
     describe("GET /calculates/pets-ages", function() {
-        it("responds with age calculation", function(done) {
+        it("responds with age calculation",async function() {
             chai.request(app)
             .get('/api/calculates/pets-ages')
             .set('x-access-token', token)
@@ -93,7 +91,6 @@ describe('Testing RESTful API', function() {
                 res.should.have.status(200);
                 res.body.totalAges.should.be.a('Number');
                 expect(res.body.totalAges).to.not.eql(0);
-                done();
             });
         });
     });
