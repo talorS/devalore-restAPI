b091027519cf563f2a34c41a98aa2a8e7744ee07
Talor Samara <Talor.Samara@sapiens.com>
2021-10-07 11:08:33 +0300

    my changes

--- models/authBL.js
+++ models/authBL.js
@@ -4,12 +4,12 @@ const accessTokenSecret = require('../configs/secretJWT');
 //generte jwt access token 
 exports.getToken = function () {
   try {
-      const accessToken = jwt.sign( '',
+      const accessToken = jwt.sign( {data: ''},
           accessTokenSecret,
           { expiresIn: process.env.JWT_EXPIRATION, }
       );
       return accessToken;
   } catch (err) {
-      console.error(err);
+      console.error(err.message);
   }
 }
